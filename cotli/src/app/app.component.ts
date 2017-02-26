9804a5f9436cd5231286efd52ac2ff710e9bfd02
Jamie <jdhatfield@gmail.com>
2017-02-25 20:38:07 -0700

    Listing out heroes

--- cotli/src/app/app.component.ts
+++ cotli/src/app/app.component.ts
@@ -1,7 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import {Observable} from 'rxjs/Rx';
+
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+    <h1>Hello {{name}}</h1>
+    <table>
+    <tr *ngFor="let hero of hero_defines">
+        <td>{{hero.name}}</td>
+    </tr>
+    </table>
+
+  `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent implements OnInit { 
+    name = 'Angular';
+    hero_defines: Object;
+    loot_defines: Object;
+    effect_defines: Object;
+
+    constructor(private http: Http) {
+    }
+
+    ngOnInit() {
+        console.log("onInit");
+        var url = '../data/cotliDefines.json';
+        this.http.get(url)
+            .map((res:Response) => res.json())
+            .catch((error:any) => Observable.throw(error.json().error || 'Server error'))
+            .subscribe(
+                cotliDefines => {
+                    console.log("cotliDefines: ");
+                    console.log(cotliDefines);
+                    this.hero_defines = cotliDefines.hero_defines;
+                    this.loot_defines = cotliDefines.loot_defines;
+                    this.effect_defines = cotliDefines.effect_defines;
+                },
+                err => {
+                    console.log("Error from subscribe: " + err):
+                }
+            );
+        console.log("after http");
+    }
+}
